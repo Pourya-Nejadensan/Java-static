b2fdcae0afeecd09586b3ef8594e637fd4a12065
Pourya <pourya.nejadensan@gmail.com>
2024-07-05 12:03:06 +0200

    write more test case for main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,12 @@ public class Main {
         MyClass myClass3 = new MyClass();
 
         myClass1.incrementInstanceCount();
+
         myClass2.incrementInstanceCount();
+        myClass2.incrementInstanceCount();
+
+        myClass3.incrementInstanceCount();
+        myClass3.incrementInstanceCount();
         myClass3.incrementInstanceCount();
 
         MyClass.incrementTotalCount();
@@ -14,12 +19,16 @@ public class Main {
 
         System.out.println(myClass1.toString());
         MyClass.incrementTotalCount();
+        MyClass.incrementTotalCount();
 
         System.out.println(myClass2);
+        MyClass.incrementTotalCount();
+        MyClass.incrementTotalCount();
         MyClass.incrementTotalCount();
 
         System.out.println(myClass3.toString());
 
+        // Bonus task
         System.out.println(Calculator.add(2,2));
         System.out.println(Calculator.subtraction(2,2));
         System.out.println(Calculator.multiplication(2,2));
