9aeb09602a3bd0f3ae36b177570ee0c9e09f1235
Pourya <pourya.nejadensan@gmail.com>
2024-07-05 11:10:45 +0200

    Step 3: Write an instance method 'incrementInstanceCount' that increments the 'instanceCount' for the object by 1.

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -1,8 +1,12 @@
 public class MyClass {
     private static int totalCount = 0;
-    private static int instanceCount = 0;
+    private int instanceCount = 0;
 
     public static void incrementTotalCount(){
         totalCount++;
+    }
+
+    public void incrementInstanceCount(){
+        instanceCount++;
     }
 }
